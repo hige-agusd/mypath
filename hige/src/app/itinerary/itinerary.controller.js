9a26b4087f9687f5074d95165c1b4dcd48fe72b4
Hige <diazagustin@gmail.com>
2015-09-28 10:42:11 -0300

    DnD working fine - data from markers - new elements in progress: dropdowns ready, elements pending

--- hige/src/app/itinerary/itinerary.controller.js
+++ hige/src/app/itinerary/itinerary.controller.js
@@ -5,6 +5,9 @@ angular.module('myPath')
     $scope.otherTags = false;
     $scope.loading = true;
     $scope.pais = 'Japan';
+    $scope.newItemDropdown = [];
+    $scope.dropdownItems = {};
+    $scope.newContainerDropdown = [];
     $scope.models = {
       selected: null,
       templates: [
@@ -15,10 +18,11 @@ angular.module('myPath')
     };
 
     function init() {
-      geoJsonSrv.getItinerary().success(function(data) {
+      geoJsonSrv.getMarkers().success(function(data) {
         $scope.otherTags = data;
         window.lala = $scope.otherTags;
-        $scope.models.dropzones = data.Itinerary;
+        //$scope.models.dropzones = data.Itinerary;
+        $scope.models.dropzones = dndData();
         console.log($scope.otherTags.Itinerary);
         $scope.itinerary = dndData();
         $scope.updateLink();
@@ -34,8 +38,48 @@ angular.module('myPath')
 
     function dndData() {
       var retVal = {};
+      retVal[$scope.pais] = [];
+      $scope.dropdownItems[$scope.pais] = {};
+      angular.forEach($scope.otherTags.Places[$scope.pais], function(puntos, ciudad) {
+        var columns = [];
+        angular.forEach(puntos, function(punto, i) {
+          columns.push({
+            id: punto.id,
+            type: 'item',
+            name: punto.properties.name,
+            amenity: punto.properties.amenity,
+            info: punto.properties.popupContent,
+            markerType: punto.properties.type
+          })
+        });
+        $scope.dropdownItems[$scope.pais][ciudad] = columns;
+        $scope.newContainerDropdown.push({
+          type: 'container',
+          id: ciudad,
+          columns: [columns]
+        });
+        retVal[$scope.pais].push({
+          type: 'container',
+          id: ciudad,
+          columns: [columns]
+        });
+      });
+      console.log(retVal);
       return retVal;
     }
+
+    $scope.frula = function() {
+
+    };
+
+    $scope.updateItemDropdown = function() {
+      console.log('lala')
+      console.log($scope.dropdownItems);
+      console.log($scope.newContainer);
+      $scope.newItemDropdown = $scope.dropdownItems[$scope.pais][$scope.newContainer];
+      console.log($scope.newItemDropdown);
+
+    };
 
     $scope.updateLink = function() {
       localStorage.setItem('data', angular.toJson($scope.itinerary));
